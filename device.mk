ac879122caab0474ac8f1e3d12eb7cac4e92bce8
Virsys Electron <virsys.electron@gmail.com>
2018-06-08 20:09:43 +0200

    cleanup

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #Inherit from vendor
-$(call inherit-product, vendor/samsung/gt510wifi/gt510wifi-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/gt510wifi/gt510wifi-vendor.mk)
 
 # Inherit from common
 $(call inherit-product, device/samsung/gte-common/device-common.mk)
@@ -9,20 +9,25 @@ LOCAL_PATH := device/samsung/gt510wifi
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
+# Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/apa/apa_settings.cfg:system/etc/apa_settings.cfg \
-    $(LOCAL_PATH)/configs/apa/sapa_feature.xml:system/etc/sapa_feature.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
+    $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/bluetooth/iop_bt.db:system/etc/bluetooth/iop_bt.db \
-    $(LOCAL_PATH)/configs/bluetooth/iop_device_list.conf:system/etc/bluetooth/iop_device_list.conf \
-    $(LOCAL_PATH)/configs/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
-    $(LOCAL_PATH)/configs/wifi/lowi.conf:system/etc/lowi.conf \
-    $(LOCAL_PATH)/configs/wifi/WCNSS_cfg.dat:system/etc/wifi/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/wifi/WCNSS_qcom_wlan_nv.bin \
-    $(LOCAL_PATH)/configs/wifi/xtwifi.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/configs/bluetooth/iop_device_list.conf:system/etc/bluetooth/iop_device_list.conf
+
+# Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/A05QF_sr544_module_info.xml:system/etc/A05QF_sr544_module_info.xml \
+    $(LOCAL_PATH)/configs/camera/A05QF_sr544_module_info.xml:system/etc/B05QF_sr544_module_info.xml \
+    $(LOCAL_PATH)/configs/camera/A05QF_sr544_module_info.xml:system/etc/C05QF_sr544_module_info.xml
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += device/samsung/gt510wifi/overlay
